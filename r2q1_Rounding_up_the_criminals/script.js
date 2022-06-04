8c69d9a79b4ab0cbfe9308abdd52da8eed480d7c
allanpaul0728 <allan.paul28@yahoo.com>
2022-06-04 13:34:43 +0000

    TGC16-DOM-PRACTICE

--- r2q1_Rounding_up_the_criminals/script.js
+++ r2q1_Rounding_up_the_criminals/script.js
@@ -1,4 +1,13 @@
 // PLACE YOUR CODE HERE
-let boxa = document.querySelectorAll('.box-a');
-    boxa.style.backgroundColor="red";
+let boxa = document.querySelectorAll('.a');
+for (let b of boxa) {
+    b.style.backgroundColor = 'red';
+}
 
+let jailed = document.querySelectorAll('.box')
+let name = innerHTML
+for (let j of jailed) {
+    // if (j == innerHTML) {
+    jailed = `${name}` + 'Jailed';
+    // }
+}
