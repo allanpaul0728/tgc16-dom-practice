5dbc5c29323e369d5123ab2df3fdc059bd4fd9c5
allanpaul0728 <allan.paul28@yahoo.com>
2022-06-04 07:39:15 +0000

    TGC16-DOM-PRACTICE

--- q6_counter/script.js
+++ q6_counter/script.js
@@ -5,30 +5,32 @@ let decrement = document.querySelector('#decrement');
 let reset = document.querySelector('#reset');
 box.style.backgroundColor = 'green';
 
+
 increment.addEventListener('click', function() {
-  if(value<10){
-    value += 1
-    box.innerHTML = value;
-    }
-    
-    
-    if(value % 2 == 0) {
-        box.style.backgroundColor="green";
-    }
-    else box.style.backgroundColor="red"
-    
+  if (box.innerHTML < 10) {
+  box.innerHTML = Number(box.innerHTML) + 1;
+  }
+  if (box.innerHTML % 2 == 0) {
+    box.style.backgroundColor = 'green';
+  } else {
+    box.style.backgroundColor = 'red';
+  }
 })
-decrement.addEventListener('click', function() {
-  if(value>0 ){
-    value -= 1
-    box.innerHTML = value;
-     }
-       if(value % 2 == 0) {
-        box.style.backgroundColor="green";
-        }
-       else box.style.backgroundColor="red";
+
+decrement.addEventListener('click', function(){
+  if (box.innerHTML > 0) {
+  box.innerHTML = Number(box.innerHTML) - 1;
+  }
+  if (box.innerHTML % 2 == 0) {
+    box.style.backgroundColor = 'green';
+  } else {
+    box.style.backgroundColor = 'red';
+  }
 })
-reset.addEventListener('click', function() {
-  value = 0
-  box.innerHTML = value;
+
+reset.addEventListener('click', function(){
+  box.innerHTML = 0;
+  if (box.innerHTML == 0)
+  box.style.backgroundColor ='green';
 })
+
--- r2q1_Rounding_up_the_criminals/script.js
+++ r2q1_Rounding_up_the_criminals/script.js
@@ -1,3 +1,4 @@
 // PLACE YOUR CODE HERE
-
+let boxa = document.querySelectorAll('.box-a');
+    boxa.style.backgroundColor="red";
 
